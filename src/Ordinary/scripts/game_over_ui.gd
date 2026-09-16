f95b83033890cc43542d978f169ec877c1e1b74c
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-09-16 08:44:40 -0300

    fix: fix sizing

--- src/Ordinary/scripts/game_over_ui.gd
+++ src/Ordinary/scripts/game_over_ui.gd
@@ -1,4 +1,9 @@
 extends Control
 
+@onready var back_ground := $ColorRect
+
+func _ready() -> void:
+	$AnimationPlayer.play("GameOver_fadein")
+
 func _on_retry_button_pressed() -> void:
 	GameManager.restart_game()
