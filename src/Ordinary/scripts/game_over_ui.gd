c51d0540c085730cc8f0261e1bc8761d0a1d44ef
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-09-17 18:18:59 -0300

    feat: add new game over interface

--- src/Ordinary/scripts/game_over_ui.gd
+++ src/Ordinary/scripts/game_over_ui.gd
@@ -2,17 +2,8 @@ extends Control
 
 
 func _ready() -> void:
-	var tip := $VisibleElements/Tip
-	
-	match GameManager.reason:
-		GameManager.Reason.TIMEOUT:
-			tip.text = "Você precisa ser mais rápido!"
-		GameManager.Reason.EXHAUSTION:
-			tip.text = "Você tentou descançar?
-			Explore o mapa"
-		
-			
 	$AnimationPlayer.play("GameOver_fadein")
 
 func _on_menu_button_pressed() -> void:
-	GameManager.go_to_main_menu()
+	#GameManager.go_to_main_menu()
+	GameManager.restart_game()
